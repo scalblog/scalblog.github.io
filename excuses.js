763d58aa126655c06fea0ae7b1ed350e2f00b489
Pascal <scalblog@yahoo.fr>
2017-03-01 21:26:18 +0100

    Update excuses.js

--- excuses.js
+++ excuses.js
@@ -1,13 +1,13 @@
 function Excuse() {
 	
-	var myDog = ['dog', 'cat', 'sense of ennui', 'hamster', 'chinchilla', 'iguana', 'turtle', 'best friend', 'bro', 'boo', 'crush', 'sister', 'brother', 'nemesis', 'doppelganger', 'gerbil', 'bunny', 'fish', 'crew'];
-	var ate = ['ate', 'peer-reviewed', 'destroyed', 'deleted', 'erased', 'remixed', 'twitched', 'recycled', 'livetweeted', 'undermined', 'underbid', 'upcycled', 'gave away', 'plagiarised', 'confiscated', 'barfed on', 'stole', 'ransomed', 'spilled water on', 'lost', 'misplaced', 'buried', 'shredded', 'hid', 'liberated', 'left with', 'ebayed', 'craigslisted', 'etsyed', 'tweeted', 'instagrammed', 'snapchatted', 'youtubed', 'dunked on', 'subteweeted', 'favorited', 'upvoted', 'downvoted', 'live cast', 'pinned', 'vlogged', 'blogged about'];
-	var myHomework = ['homework', 'project', 'essay', 'film project', 'visualization', 'infographic', 'pre-writing', 'term paper', 'lab report', 'reading log', 'summary', 'article', 'flash cards', 'study guide', 'textbook', 'workbook', 'reading book', 'diorama', 'poster', 'dreams', 'laptop', 'computer', 'tablet', 'momentum', 'report', 'slideshow', 'slide deck', 'presentation', 'worksheet', 'handout', 'notes', 'portfolio', 'blog post', 'comment'];
+	var myDog = ['chien', 'chat', 'théorie sur pythagore', 'version de Windows', 'iguane', 'navigateur pas vraiment à jour', 'avatar', 'voisin', 'complot autour de  Matrix','artefact', 'double maléfique', 'poisson rouge', 'historique dans Google'];
+	var ate = ['a écrasé', 'a remplacé', 'a effacé', 'a altéré', 'a détruit', 'a modifié irrémédiablement', 'a inversé', 'a recyclé', 'a tweeté sur ', 'a recalculé', 'a reparamétré', 'a remis à jour', 'a abandonné', 'a plagié', 'a isolé', 'a volé', 'a trié aléatoirement', 'a renversé de l\'eau sur', 'a perdu', 'a mal interprété', 'a incinéré', 'a fait du tri sélectif sur', 'a caché', 'a reparamétré', 'a mis de côté', 'a mis sur eBay', 'a ajouté en favori sur Facebook', 'a mis sur SnapChat', 'a mis sur Youtube', 'a bu pendant que vous bossiez sur', 'n\'a pas liké', 'a acheté sur Amazon'];
+	var myHomework = ['projet', 'navigateur', 'travail', 'session', 'fichier Excel', 'document Word', 'papier', 'contrat', 'tentative de connexion', 'blog', 'article', 'carte mémoire', 'aide en ligne', 'rêve d\'un monde meilleur', 'memento', 'compte-rendu', 'fichier dans le Cloud', 'portfolio', 'commentaire'];
 	
 	var who = myDog[Math.round(Math.random()*(myDog.length-1))];
 	var did = ate[Math.round(Math.random()*(ate.length-1))];
 	var what = myHomework[Math.round(Math.random()*(myHomework.length-1))];
 	
-	document.getElementById('excuses').innerHTML = '<div>My ' + who + ' ' + did + '<br> my ' + what + '.</div>'
+	document.getElementById('excuses').innerHTML = '<div>En fait... c\'est votre ' + who + ' qui ' + did + '<br> votre ' + what + '.</div>'
 	
 }
