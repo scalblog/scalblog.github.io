8633f4502f7a037cb1a9442eafdda8d21b773504
Pascal <scalblog@yahoo.fr>
2017-03-09 22:18:01 +0100

    Update excuses.js

--- excuses.js
+++ excuses.js
@@ -1,13 +1,14 @@
-function Excuse() {
-	
-	var myDog = ['chien', 'chat', 'théorie sur pythagore', 'version de Windows', 'iguane', 'navigateur pas vraiment à jour', 'avatar', 'voisin', 'complot autour de  Matrix','artefact', 'double maléfique', 'poisson rouge', 'historique dans Google'];
-	var ate = ['a écrasé', 'a remplacé', 'a effacé', 'a altéré', 'a détruit', 'a modifié irrémédiablement', 'a inversé', 'a recyclé', 'a tweeté sur ', 'a recalculé', 'a reparamétré', 'a remis à jour', 'a abandonné', 'a plagié', 'a isolé', 'a volé', 'a trié aléatoirement', 'a renversé de l\'eau sur', 'a perdu', 'a mal interprété', 'a incinéré', 'a fait du tri sélectif sur', 'a caché', 'a reparamétré', 'a mis de côté', 'a mis sur eBay', 'a ajouté en favori sur Facebook', 'a mis sur SnapChat', 'a mis sur Youtube', 'a bu pendant que vous bossiez sur', 'n\'a pas liké', 'a acheté sur Amazon'];
-	var myHomework = ['projet', 'navigateur', 'écran pendant sa veille', 'session', 'fichier Excel', 'document Word', 'papier', 'contrat', 'tentative de connexion', 'blog', 'article', 'carte mémoire', 'aide en ligne', 'rêve d\'un monde meilleur', 'memento', 'compte-rendu', 'fichier dans le Cloud', 'portfolio', 'commentaire'];
-	
-	var who = myDog[Math.round(Math.random()*(myDog.length-1))];
-	var did = ate[Math.round(Math.random()*(ate.length-1))];
-	var what = myHomework[Math.round(Math.random()*(myHomework.length-1))];
-	
-	document.getElementById('excuses').innerHTML = '<div>En fait... c\'est votre ' + who + ' qui ' + did + '<br> votre ' + what + '.</div>'
-	
+function excuse() {
+  
+  var fautif = ['chien', 'chat', 'théorie sur pythagore', 'version de Windows', 'iguane', 'navigateur pas vraiment à jour', 'avatar', 'voisin', 'complot autour de  Matrix','artefact', 'double maléfique', 'poisson rouge', 'historique dans Google'];
+	var commet = ['a écrasé', 'a remplacé', 'a effacé', 'a altéré', 'a détruit', 'a modifié irrémédiablement', 'a inversé', 'a recyclé', 'a tweeté sur ', 'a recalculé', 'a reparamétré', 'a remis à jour', 'a abandonné', 'a plagié', 'a isolé', 'a volé', 'a trié aléatoirement', 'a renversé de l\'eau sur', 'a perdu', 'a mal interprété', 'a incinéré', 'a fait du tri sélectif sur', 'a caché', 'a reparamétré', 'a mis de côté', 'a mis sur eBay', 'n\'avait pas ajouté en favori sur Facebook', 'n\'a pas aimé sur SnapChat', 'n\'a pas aimé sur Youtube', 'a bu pendant que vous bossiez sur', 'n\'a pas liké', 'a acheté sur Amazon', 'a mal lu'];
+	var faute = ['projet', 'navigateur', 'travail', 'session', 'fichier Excel', 'document Word', 'papier', 'contrat', 'tentative de connexion', 'blog', 'article', 'carte mémoire', 'aide en ligne', 'rêve d\'un monde meilleur', 'memento', 'compte-rendu', 'fichier dans le Cloud', 'portfolio', 'commentaire'];
+
+var qui = fautif[Math.round(Math.random()*(fautif.length-1))];
+var fait = commet[Math.round(Math.random()*(commet.length-1))];
+var quoi = faute[Math.round(Math.random()*(faute.length-1))];
+
+ document.getElementById('excuse').innerHTML = '<p>En fait... c\'est votre ' + qui + ' qui ' + fait + '<br> votre ' + quoi + '.</p>';
 }
+
+document.getElementById('generator').addEventListener('click',excuse);
